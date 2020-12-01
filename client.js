c16f54d3de2ef75ab195bea8506a3f6d12d19009
saoussen <sawsan_sli3i@live.fr>
2020-12-01 15:57:14 +0000

    imported the constants in client.js

--- client.js
+++ client.js
@@ -1,8 +1,9 @@
 const net = require('net');
+const {IP, PORT} = require('./constants')
 const connect = function() {
   const conn = net.createConnection({ 
-    host: '10.0.2.15',
-    port: 50541
+    host: IP,
+    port: PORT
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 

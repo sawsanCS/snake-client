96dacdcbb6e7d813c1aef5de98f4e855851c8777
saoussen <sawsan_sli3i@live.fr>
2020-12-01 15:14:45 +0000

    implemented handleUserInput to handle Ctrl C input and terminate process

--- play.js
+++ play.js
@@ -3,3 +3,22 @@ const connect = require('./client');
 
 console.log('Connecting ...');
 connect();
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+}
+
+const handleUserInput = function(data) {
+  if (data === '\u0003') {
+    process.exit();
+  }
+};
+setupInput();

3c366ae6fa57bcf3ba3e5bf75ff5bce600be6aa4
saoussen <sawsan_sli3i@live.fr>
2020-12-01 14:45:19 +0000

    required the connect from client.js

--- play.js
+++ play.js
@@ -1,21 +1,10 @@
 const net = require('net');
+const connect = require('./client');
 
 /**
  * Establishes connection with the game server
  */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '10.0.2.15',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-    });
-    
-  return conn;
-}
+
 
 console.log('Connecting ...');
 connect();

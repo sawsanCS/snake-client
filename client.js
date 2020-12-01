9e2d9f81cbd225590418cc1be3a8c80f98ae8f20
saoussen <sawsan_sli3i@live.fr>
2020-12-01 14:44:38 +0000

    implemented client.js by moving connect to it

---
+++ client.js
@@ -0,0 +1,14 @@
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '10.0.2.15',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+    });
+    
+  return conn;
+}
+module.exports = connect;

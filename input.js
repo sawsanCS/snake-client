aed8590777d4bfb48c0414ade9bdabfa75d7b930
saoussen <sawsan_sli3i@live.fr>
2020-12-01 15:28:39 +0000

    added the buttons w a s d to the handler

--- input.js
+++ input.js
@@ -11,5 +11,21 @@ const handleUserInput = function(data) {
   if (data === '\u0003') {
     process.exit();
   }
+  if (data === 'w') {
+    conn.write('Move: up');
+    console.log('up');
+  }
+  if (data === 'a') {
+    conn.write('Move: left');
+    console.log('left');
+  }
+  if (data === 's') {
+    conn.write('Move: down');
+    console.log('down');
+  }
+  if (data === 'd') {
+    conn.write('Move: right');
+    console.log('right');
+  }
 };
 module.exports = setupInput, handleUserInput;

418e97a6400cbd34b0abb5fdb7c4c7779fa3fab9
saoussen <sawsan_sli3i@live.fr>
2020-12-01 15:06:47 +0000

    added setTimeout to see the snake move

--- client.js
+++ client.js
@@ -10,13 +10,14 @@ const connect = function() {
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
       conn.write('Name: SSM');
-      conn.write("Move: up");
-      conn.write("Move: up");
-      conn.write("Move: left");
-      conn.write("Move: up");
-      conn.write("Move: right");
-      conn.write("Move: down");
-      conn.write("Move: down");
+      setTimeout(() => {conn.write("Move: up");} , 1000)
+      setTimeout(() => {conn.write("Move: up");} , 2000)
+      setTimeout(() => {conn.write("Move: left");} , 3000)
+      setTimeout(() => {conn.write("Move: left");} , 4000)
+      setTimeout(() => {conn.write("Move: down");} , 5000)
+      setTimeout(() => {conn.write("Move: down");} , 6000)
+      setTimeout(() => {conn.write("Move: right");} , 7000)
+      
     });
   conn.on('data', (data) => {
     console.log('Server says: ', data);

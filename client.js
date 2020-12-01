88edb4fcd9c5b220299fb642dcefbf186a29179c
saoussen <sawsan_sli3i@live.fr>
2020-12-01 14:52:46 +0000

    added a console log to indicate that the connection is established

--- client.js
+++ client.js
@@ -6,6 +6,10 @@ const connect = function() {
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
+
+  conn.on('connect', () => {
+    console.log('connection established');
+    });
   conn.on('data', (data) => {
     console.log('Server says: ', data);
     });

92b9b827a84dfa8348121855541683c518dee1db
saoussen <sawsan_sli3i@live.fr>
2020-12-01 15:22:14 +0000

    refactored play.js to require other modules

--- play.js
+++ play.js
@@ -1,24 +1,11 @@
 
 const connect = require('./client');
-
+const {setupInput, handleUserInput} = require ('./input')
 console.log('Connecting ...');
 connect();
 /**
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', handleUserInput);
-  return stdin;
-}
 
-const handleUserInput = function(data) {
-  if (data === '\u0003') {
-    process.exit();
-  }
-};
-setupInput();
+setupInput;
